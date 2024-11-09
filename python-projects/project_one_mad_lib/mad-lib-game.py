77c19caa74777c8eeaa58a7797de74b7228e0f35
HiDamienDesai <131928836+HiDhavalDesai@users.noreply.github.com>
2024-11-08 22:02:56 -0600

    learning python

---
+++ python-projects/project_one_mad_lib/mad-lib-game.py
@@ -0,0 +1,20 @@
+# Welcome to a Mad Libs game. This game will ask you questions a few questions and then it will print out a story.
+# Please follow the instructions on the screen.
+
+adjective_one =
+noun_one =
+
+Print(f"Today, I decided to visit the park to enjoy the beautiful {adjective_one} weather.")
+Print(f"I packed my {noun_one} and headed out.")
+Print
+
+
+
+
+When I arrived, I saw a (animal) that was (verb, ending in -ing) by the (noun)! It looked (adjective).
+
+I then decided to join a group of people who were (verb, ending in -ing) a game of (sport). I wasn’t very good, but I tried my best. After a few rounds, I took a break and enjoyed a delicious (food) from a nearby stand. It tasted (adjective)!
+
+While relaxing, I noticed a group of (plural noun) having a (adjective) time near the (body of water). They were (verb, ending in -ing) and seemed to be having so much fun! As the sun began to set, I felt (emotion) and decided it was time to head home.
+
+I will definitely come back to this (adjective) park again soon!
