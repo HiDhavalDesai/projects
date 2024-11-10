437adbf4664a70cb07d437c3ba30162567934ba8
HiDamienDesai <131928836+HiDhavalDesai@users.noreply.github.com>
2024-11-10 17:27:55 -0600

    Finished my mad libs game.

--- python-projects/project_one_mad_lib/mad-lib-game.py
+++ python-projects/project_one_mad_lib/mad-lib-game.py
@@ -1,20 +1,29 @@
 # Welcome to a Mad Libs game. This game will ask you questions a few questions and then it will print out a story.
 # Please follow the instructions on the screen.
 
-adjective_one =
-noun_one =
+adjective_one = input("Please enter an Adjective: ")
+noun_one = input("Please enter a Noun: ")
+animal = input("Please enter an Animal: ")
+verb_one = input("Please enter a Verb ending in '-ing': ")
+noun_two = input("Please enter a Noun: ")
+adjective_two = input("Please enter an Adjective: ")
+verb_two = input("Please enter a Verb ending in '-ing': ")
+sport = input("Please enter a Sport: ")
+food = input("Please enter a food: ")
+adjective_three = input("Please enter an Adjective: ")
+noun_three = input("Please enter a plural Noun: ")
+adjective_four = input("Please enter an Adjective: ")
+body_of_water = input("Please enter the name of a Body of Water: ")
+verb_three = input("Please enter a Verb ending in '-ing': ")
+emotion = input("Please enter an Emotion: ")
+adjective_five = input("Please enter an Adjective: ")
 
-Print(f"Today, I decided to visit the park to enjoy the beautiful {adjective_one} weather.")
-Print(f"I packed my {noun_one} and headed out.")
-Print
 
-
-
-
-When I arrived, I saw a (animal) that was (verb, ending in -ing) by the (noun)! It looked (adjective).
-
-I then decided to join a group of people who were (verb, ending in -ing) a game of (sport). I wasn’t very good, but I tried my best. After a few rounds, I took a break and enjoyed a delicious (food) from a nearby stand. It tasted (adjective)!
-
-While relaxing, I noticed a group of (plural noun) having a (adjective) time near the (body of water). They were (verb, ending in -ing) and seemed to be having so much fun! As the sun began to set, I felt (emotion) and decided it was time to head home.
-
-I will definitely come back to this (adjective) park again soon!
+print(f"Today, I decided to visit the park to enjoy the beautiful {adjective_one} weather.")
+print(f"I packed my {noun_one} and headed out.")
+print(f"When I arrived, I saw an {animal} that was {verb_one} by the {noun_two}! It looked {adjective_two}")
+print(f"I then decided to join a group of people who were {verb_two} a game of {sport}.")
+print(f"I wasn't very good, but I tried my best. After a few rounds. I took a break and enjoyed a delicous {food} from a nearby stand. It tasted {adjective_three}!")
+print(f"While relaxing, I noticed a group of {noun_three} having a {adjective_four} times near the {body_of_water}.")
+print(f"They were {verb_three} and seemed to be having so much fun! As the sun began to set, I felt {emotion} and decided it was time to head home.")
+print(f"I will definitely come back to this {adjective_five} park again soon!")
